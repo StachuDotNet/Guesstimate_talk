45234bcb810b3368504742d6d47519c3ccd21dac
Stachu Korick <StachuKorick@gmail.com>
2014-08-15 00:36:29 -0400

    More stuff

---
+++ RavenDB/End/RavenDb_Demo/Web/HomeModule.cs
@@ -0,0 +1,44 @@
+﻿using Nancy;
+using Raven.Client.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web
+{
+    public class HomeModule : NancyModule
+    {
+        DocumentStore docStore;
+
+        public HomeModule()
+        {
+            docStore = new DocumentStore { Url = "http://localhost:8081//" };
+            docStore.Initialize();
+
+            Get["/"] = _ =>
+            {
+                var session = docStore.OpenSession();
+
+                return Response.AsJson(session.Query<Person>().ToList());
+            };
+
+            Get["/add/{name}"] = ctx =>
+            {
+                var session = docStore.OpenSession();
+                
+                var entity = new Person{Name = ctx.name};
+                session.Store(entity);
+                session.SaveChanges();
+
+                return Response.AsText(entity.Id.ToString()).WithStatusCode(HttpStatusCode.Created);
+            };
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
