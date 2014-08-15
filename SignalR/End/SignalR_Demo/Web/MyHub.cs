c5c152a321bb1bf2e767826d3f6cf92bfeafc1bf
Stachu Korick <StachuKorick@gmail.com>
2014-08-14 22:34:46 -0400

    SignalR end 'done'

---
+++ SignalR/End/SignalR_Demo/Web/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Web
+{
+    public class MyHub : Hub
+    {
+        public void ServerFunc(string serverParam)
+        {
+            Clients.All.ClientFunc(serverParam + ", back from the server.");
+        }
+    }
+}
